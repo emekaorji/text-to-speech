72704cf0ae68728624f3e6c0259b080a5f98fccd
Emeka Orji <emekapraiseo@gmail.com>
2021-12-20 14:48:25 +0100

    Tweaked somethings

--- speech.js
+++ speech.js
@@ -3,14 +3,17 @@ const counter = document.querySelector(".speech");
 var playButton = document.querySelector('#play');
 playButton.addEventListener("click", () => {
   setInterval(() => {
-    if (count > 0) {
+    if (count > -1) {
       counter.innerHTML = count--;
       if ('speechSynthesis' in window) with (speechSynthesis) {
         utterance = new SpeechSynthesisUtterance(counter.textContent);
         utterance.voice = getVoices()[1];
-        utterance.rate = 4;
+        utterance.rate = 2.8;
+        if (count > 100) {
+          utterance.rate = 3.5;
+        }
         speak(utterance);
       }
     }
-  }, 2000);
+  }, 1500);
 });
